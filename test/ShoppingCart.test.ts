da9ca314483185f662764bafcecf9ff5a62438b9
Olaf Thielke <olaf.thielke@gmail.com>
2022-04-20 16:36:32 +1200

    When construct cart it must be empty - Red (Compile)

--- test/ShoppingCart.test.ts
+++ test/ShoppingCart.test.ts
@@ -5,4 +5,9 @@ describe("When construct ShoppingCart", () => {
     it("Then constructs a new cart", () => {
         const cart = new ShoppingCart();
     });
+
+    it("Then the cart must be empty.", () => {
+        const cart = new ShoppingCart();
+        expect(cart.items).toEqual([])
+    });    
 });
