c6e60a88660e18fe86dcba9278683d83a123cc60
Olaf Thielke <olaf.thielke@gmail.com>
2022-04-20 16:20:57 +1200

    Can Construct ShoppingCart - Green

--- test/ShoppingCart.test.ts
+++ test/ShoppingCart.test.ts
@@ -1,3 +1,5 @@
+import ShoppingCart from "../src/ShoppingCart";
+
 describe("When Construct ShoppingCart", () => {
     it("Then Constructs a new Cart", () => {
         const cart = new ShoppingCart();
