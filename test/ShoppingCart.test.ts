44de102a6026c1504bcb9233ebbd27a44a36fd8e
Olaf Thielke <olaf.thielke@gmail.com>
2022-04-20 16:42:15 +1200

    When construct cart it must have total of 0 - Red (Compile)

--- test/ShoppingCart.test.ts
+++ test/ShoppingCart.test.ts
@@ -2,12 +2,17 @@ import ShoppingCart from "../src/ShoppingCart";
 
 describe("When construct ShoppingCart", () => {
 
-    it("Then constructs a new cart", () => {
+    it("Then constructs a new cart.", () => {
         const cart = new ShoppingCart();
     });
 
     it("Then the cart must be empty.", () => {
         const cart = new ShoppingCart();
         expect(cart.items).toEqual([])
-    });    
+    });
+
+    it("Then cart.total must be 0.", () => {
+        const cart = new ShoppingCart();
+        expect(cart.total).toBe(0);
+    });
 });
