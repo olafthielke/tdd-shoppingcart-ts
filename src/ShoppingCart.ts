daa6ad142da215de4ac83812f158c8662044d180
Olaf Thielke <olaf.thielke@gmail.com>
2022-04-21 16:22:48 +1200

    Call cart.add() with 0 quantity throw ZeroQuantity - Green

--- src/ShoppingCart.ts
+++ src/ShoppingCart.ts
@@ -1,8 +1,10 @@
+import { ZeroQuantity } from "./errors";
+
 export default class ShoppingCart {
     items: any = [];
     total: number = 0;
 
     public add(product: any, quantity: number) {
-
+        throw new ZeroQuantity();
     }
 }
