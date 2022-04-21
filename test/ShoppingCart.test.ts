11072eb13bf0209b25cbd79888afb25ff74a8239
Olaf Thielke <olaf.thielke@gmail.com>
2022-04-21 21:01:46 +1200

    Call cart.add() with -1 quantity throw NegativeQuantity with message - Refactor

--- test/ShoppingCart.test.ts
+++ test/ShoppingCart.test.ts
@@ -25,13 +25,17 @@ describe("When call cart.add()", () => {
     it("with quantity of -1 Then throw NegativeQuantity error.", () => {
         const cart = new ShoppingCart();
         const add = () => cart.add({ }, -1);
-        expect(add).toThrow(new NegativeQuantity());
-        expect(add).toThrow("-1 is not a valid quantity.");
+        verifyNegativeQuantityError(add);
     });
 
 
     function verifyZeroQuantityError(add: () => void) {
         expect(add).toThrow(new ZeroQuantity());
         expect(add).toThrow("0 is an invalid quantity.");
+    }
+
+    function verifyNegativeQuantityError(add: () => void) {
+        expect(add).toThrow(new NegativeQuantity());
+        expect(add).toThrow("-1 is not a valid quantity.");
     }
 });
